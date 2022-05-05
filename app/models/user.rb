c202f7f9b086c015ab09125a94a7a97369797cef
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-05-05 20:53:43 +0900

    Userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true,
+            length: {
+              maxmum: 30,
+              allow_blank: true
+            }
+
+  VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
+
+  validates :password, presence: true,
+            length: {
+              minimum: 8,
+              allow_blank: true
+            },
+            format: {
+              with: VALID_PASSWORD_REGEX,
+              allow_blank: true
+            },
+            allow_nil: true
 end
