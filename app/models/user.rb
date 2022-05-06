78ec603f6c6420e512c0a98db97bcf6ebb9c5d12
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-05-06 11:17:11 +0900

    バリデーションメッセージの日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ApplicationRecord
             },
             format: {
               with: VALID_PASSWORD_REGEX,
+              message: :invalid_password,
               allow_blank: true
             },
             allow_nil: true
