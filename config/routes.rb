700531c938e89e3ef6429c706d08253f8ade45ef
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-05-06 18:04:48 +0900

    Users Controller作成及びルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       # api test action
-      resources :hello, only: %i[index]
+      resources :users, only:[:index]
     end
   end
 end
